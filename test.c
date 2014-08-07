aeb584204cf3483560d31b85829850ff83cc25e5
Benyjuice <fuxixi1991@163.com>
2014-08-07 20:21:54 +0800

    msp430 program tset

---
+++ test.c
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test.c
+ *
+ *    Description:  This is a test of github file
+ *
+ *        Version:  1.0
+ *        Created:  2014年08月07日 20时20分05秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Beny Fu (B.F.), fuxixi1991@163.com
+ *        Company:  No Copyright Yet
+ *
+ * =====================================================================================
+ */
+
+
